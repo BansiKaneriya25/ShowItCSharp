2d68ff01c448d1368efa0e50d2dff3539801025a
RajnikantSerasiya <rserasiya@gmail.com>
2024-05-06 07:07:25 +0530

    command line arg changes

--- ConsoleApp3/ConsoleApp3/CommandLineArgument.cs
+++ ConsoleApp3/ConsoleApp3/CommandLineArgument.cs
@@ -1,16 +1,16 @@
-﻿//using System;
-//using System.IO;
+﻿using System;
+using System.IO;
 
-//namespace ConsoleApp3
-//{
-//    class CommandLineArgument
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine($"First Arg: {args[0]}");
-//            //Need to connect with DB
-//            string connectionString = args[0].ToString();
-//            Console.ReadLine();
-//        }
-//    }
-//}
+namespace ConsoleApp3
+{
+    class CommandLineArgument
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"First Arg: {args[0]}");
+            //Need to connect with DB
+            string connectionString = args[0].ToString();
+            Console.ReadLine();
+        }
+    }
+}
